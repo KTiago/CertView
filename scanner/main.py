97a7df396064578916c64e7c1d8e1cfdf305dae4
KTiago <tiago.kieliger@gmail.com>
2020-04-30 06:43:21 -0400

    Implemented loop to run scans continuously one after the other

--- scanner/main.py
+++ scanner/main.py
@@ -1,6 +1,9 @@
 import asyncio
 import json
 import datetime
+import subprocess
+import time
+from multiprocessing import Process, Pipe
 from helpers.producer import KafkaProducer
 
 async def computation(producer, raw, date):
@@ -9,11 +12,10 @@ async def computation(producer, raw, date):
         certificate = data['data']['tls']['result']['handshake_log']['server_certificates']['certificate']['raw']
         #print(certificate)
         result = await producer.produce("scan", {"date":date, "data":data})
-        print("result ", result.result())
     except Exception:
         pass
 
-async def read(producer):
+async def read(producer, child_conn):
     date = datetime.datetime.now().strftime("%Y-%m-%d")
     with open('/home/user/Project/scan/certificates.txt') as f:
         while True:
@@ -21,19 +23,32 @@ async def read(producer):
             if raw:
                asyncio.create_task(computation(producer, raw, date))
             else:
+                if child_conn.poll(): # Scan is done, and no new line to read
+                    break
                 await asyncio.sleep(0.5)
 
-def main():
+def run_producer(child_conn):
+    print("Start producing")
     config = {"bootstrap.servers": "localhost:9092"}
     loop = asyncio.get_event_loop()
     producer = KafkaProducer(config, loop)
-    loop.run_until_complete(loop.run_until_complete(read(producer)))
+    loop.run_until_complete(read(producer, child_conn))
+    time.sleep(60)
+    loop.close()
+    producer()
+    print("Done producing")
 
+def main():
+    while True:
+        print("Scan started")
+        proc = subprocess.Popen("zmap -r 50000 --sender-threads=2 --cores=0,1 -p 443 -n 1% -o - | ztee hosts.txt | ./zgrab2 tls -o certificates.txt --gomaxprocs=4 --senders=4000", shell=True)
+        parent_conn, child_conn = Pipe()
+        p = Process(target=run_producer, args=(child_conn,))
+        p.start()
+        proc.wait()
+        parent_conn.send("done")
+        p.join()
+        print("Scan ended")
 
 if __name__ == "__main__":
     main()
-
-# Some commands
-# cd /home/user/Project/scan
-# zmap -B 10K -p 443 -n 0.01% -o - | ztee hosts.txt | ./zgrab2 tls -o certificates.txt
-# python3 /home/user/Project/certwatch/main.py
