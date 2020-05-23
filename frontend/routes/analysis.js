1c34630dc8fd10b4fcd657571c46cf3d4b485cf8
KTiago <tiago.kieliger@gmail.com>
2020-05-23 13:17:14 -0400

    Add search bar for certificates

--- frontend/routes/analysis.js
+++ frontend/routes/analysis.js
@@ -5,7 +5,10 @@ var router = express.Router();
 router.get('/', async function(req, res, next) {
   var data = await elastic.searchAll("icedid")
   console.log(data)
-  res.render('analysis', {data : data, tag : "icedid"});
+  res.render('analysis',
+      {data : data,
+        tag : "icedid"
+      });
 });
 
 module.exports = router;
--- frontend/routes/certificates.js
+++ frontend/routes/certificates.js
@@ -2,14 +2,23 @@ var express = require('express');
 var elastic = require('../modules/elasticUtil')
 var router = express.Router();
 
-router.get('/:sha1', async function(req, res, next) {
-  var sha1 = req.params.sha1
-  var result = await elastic.get("certificates", sha1)
-  if (result === null){
+router.get('/', async function(req, res, next) {
+  var sha1 = req.query.sha1
+  if (sha1 === undefined){
+    res.render('certificates')
+  } else if(sha1.length != 40){
     res.status(404)
-        .send('Not found');
+    res.render('certificate_not_found')
   }else{
-    res.send(result);
+    var certificate = await elastic.get("certificates", sha1)
+    if (certificate === null){
+      res.status(404)
+      res.render('certificate_not_found')
+    }else{
+      res.render('certificate', {
+        certificate: certificate
+      });
+    }
   }
 });
 
